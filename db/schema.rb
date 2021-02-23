473361efc0e605e4ff52ebef8de027eb05a838a0
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-02-23 19:49:14 +0100

    body je zdj text

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_131524) do
+ActiveRecord::Schema.define(version: 2021_02_23_182936) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
