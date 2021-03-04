07774411ab06abd411fb3cd16316ebfe27144a39
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-03-04 17:09:25 +0100

    dodajanje imena userju

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: %i[ index show ]
-  before_action :authorize_user!, except: %i[ index show ]
+  before_action :authorize_user!, only: %i[ edit update destroy ]
 
   # GET /posts or /posts.json
   def index
@@ -71,7 +71,6 @@ class PostsController < ApplicationController
     end
 
     def authorize_user!
-      redirect_back fallback_location: root_path, alert:'Nimate dostopa do te strani!' unless current_user == @post.user
-
+      redirect_back fallback_location: root_path, alert:'Nimate dovoljenja do te akcije!' unless current_user == @post.user
     end
 end
