e4a9bb460c2af109d898d531ec2dae6ce783e92f
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-03-07 17:53:18 +0100

    _custom.scss file dodan registracija pa prijava mata placeholderje v text fieldih

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,12 +8,12 @@ end
   def display_post_show_links(post)
     capture do
     if current_user == post.user
-      concat link_to "Edit", edit_post_path(post)
+      concat link_to "Uredi", edit_post_path(post) 
       concat ' | '
-      concat link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
+      concat link_to 'Zbriši', post, method: :delete, data: { confirm: 'Are you sure?' }
       concat ' | '
     end
-    concat link_to "Back", posts_path
+    concat link_to "Nazaj", posts_path
   end
   end
 end
