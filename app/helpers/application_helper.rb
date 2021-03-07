81b96acae2d969029712211a338ce9e452a8e8fb
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-03-07 13:09:18 +0100

    application.html.erb - nav class(drop down meni) application_helper.rb - class:"dropdown-item"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
   def display_session_links
     capture do
       if user_signed_in?
-        concat link_to "Uredi profil", edit_user_registration_path
+        concat link_to "Uredi profil", edit_user_registration_path, class:"dropdown-item"
         concat " "
-        concat link_to "Odjava", destroy_user_session_path, method: :delete
+        concat link_to "Odjava", destroy_user_session_path, method: :delete, class:"dropdown-item"
       else
-        concat link_to "Prijava", new_user_session_path
+        concat link_to "Prijava", new_user_session_path, class:"dropdown-item"
         concat " "
-        concat link_to "Registracija", new_user_registration_path
+        concat link_to "Registracija", new_user_registration_path, class:"dropdown-item"
       end
     end
   end
