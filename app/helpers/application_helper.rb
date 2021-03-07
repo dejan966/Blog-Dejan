b5f8a252bdbe6281dbbe224e70ea5c554dddbc8d
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-03-07 16:38:05 +0100

    index.html.erb - posti so v boxu posts_helper.rb - link show(stretched-link)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
   def display_session_links
     capture do
       if user_signed_in?
-        concat link_to "Uredi profil", edit_user_registration_path, class:"dropdown-item"
+        concat link_to "Uredi profil", edit_user_registration_path, class:"dropdown-item text-primary"
         concat " "
-        concat link_to "Odjava", destroy_user_session_path, method: :delete, class:"dropdown-item"
+        concat link_to "Odjava", destroy_user_session_path, method: :delete, class:"dropdown-item text-primary"
       else
-        concat link_to "Prijava", new_user_session_path, class:"dropdown-item"
+        concat link_to "Prijava", new_user_session_path, class:"dropdown-item text-primary"
         concat " "
-        concat link_to "Registracija", new_user_registration_path, class:"dropdown-item"
+        concat link_to "Registracija", new_user_registration_path, class:"dropdown-item text-primary"
       end
     end
   end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,19 +1,19 @@
 module PostsHelper
   def display_post_links(post)
-    html = "<td>#{link_to 'Show', post }</td>"
-    if current_user == post.user
-      html += "<td>#{link_to 'Edit', edit_post_path(post)} </td>"
-      html += "<td>#{link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
-    end
-    html.html_safe
+    capture do
+      concat link_to 'Preberi več', post, class:"stretched-link"
   end
+end
 
   def display_post_show_links(post)
-    html = ""
+    capture do
     if current_user == post.user
-      html += link_to "Edit", edit_post_path(post)
-      html += ' | '
+      concat link_to "Edit", edit_post_path(post)
+      concat ' | '
+      concat link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
+      concat ' | '
     end
-    html.html_safe
+    concat link_to "Back", posts_path
+  end
   end
 end
